82737c9361f90e25c1eec53d5822b7322500572e
him4318 <31626936+him4318@users.noreply.github.com>
2018-06-20 23:14:17 +0530

    Added negative tests only

---
+++ src/test/java/Alltest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.qainfotech.tap.training.snl.api.GameInProgressException;
+import com.qainfotech.tap.training.snl.api.InvalidTurnException;
+import com.qainfotech.tap.training.snl.api.MaxPlayersReachedExeption;
+import com.qainfotech.tap.training.snl.api.NoUserWithSuchUUIDException;
+import com.qainfotech.tap.training.snl.api.PlayerExistsException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import org.json.JSONException;
+import org.json.JSONObject;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author HIM
+ */
+public class Alltest {
+
+    BoardTest boardtest;
+
+    public Alltest() throws IOException {
+    }
+
+    @Test(expectedExceptions = {JSONException.class})
+    public void test_roll_dice_without_registering_any_user() throws InvalidTurnException, FileNotFoundException, UnsupportedEncodingException {
+        boardtest.roll_dice_without_registering_any_user();
+    }
+
+    @Test(expectedExceptions = {NullPointerException.class})
+    public void test_delete_user_without_registering_anybody() throws NoUserWithSuchUUIDException, FileNotFoundException, UnsupportedEncodingException {
+        boardtest.delete_user_without_registering_anybody();
+    }
+
+    @Test(expectedExceptions = {MaxPlayersReachedExeption.class})
+    public void test_add_more_than_four_user() throws PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException {
+        boardtest.add_more_than_four_user();
+    }
+
+    @Test(expectedExceptions = {PlayerExistsException.class})
+    public void test_add_same_name_user() throws PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException {
+        boardtest.add_same_name_user();
+    }
+    @AfterMethod
+    public void teardown() {
+        boardtest = null;
+        System.gc();
+    }
+    
+    @BeforeMethod
+    public void createobject() throws IOException
+    {
+        boardtest = new BoardTest();
+    }
+    
+    
+}
---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,79 @@
+
+import com.qainfotech.tap.training.snl.api.Board;
+import com.qainfotech.tap.training.snl.api.GameInProgressException;
+import com.qainfotech.tap.training.snl.api.InvalidTurnException;
+import com.qainfotech.tap.training.snl.api.MaxPlayersReachedExeption;
+import com.qainfotech.tap.training.snl.api.NoUserWithSuchUUIDException;
+import com.qainfotech.tap.training.snl.api.PlayerExistsException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import org.testng.annotations.Test;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.testng.Assert;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author HIM
+ */
+public class BoardTest {
+
+    Board board;
+
+    public BoardTest() throws UnsupportedEncodingException, IOException {
+        board = new Board();
+    }
+
+    public JSONObject roll_dice_without_registering_any_user() throws InvalidTurnException, FileNotFoundException, UnsupportedEncodingException {
+        return board.rollDice(null);
+
+    }
+
+    public JSONArray delete_user_without_registering_anybody() throws NoUserWithSuchUUIDException, FileNotFoundException, UnsupportedEncodingException {
+        return board.deletePlayer(null);
+    }
+
+    public void add_user(String name, Boolean add_i) throws PlayerExistsException, GameInProgressException, UnsupportedEncodingException, MaxPlayersReachedExeption, IOException {
+        int before_player_count, after_player_count;
+        if (add_i) {
+            for (int i = 0; i < 6; i++) {
+                before_player_count = board.getData().getJSONArray("players").length();
+                board.registerPlayer(name + i);
+                after_player_count = board.getData().getJSONArray("players").length();
+                if (after_player_count < before_player_count) {
+                    Assert.assertTrue(false, "player count does not increase in JSON");
+                }
+            }
+        } else {
+            for (int i = 0; i < 6; i++) {
+                before_player_count = board.getData().getJSONArray("players").length();
+                board.registerPlayer(name);
+                after_player_count = board.getData().getJSONArray("players").length();
+                if (after_player_count < before_player_count) {
+                    Assert.assertTrue(false, "player count does not increase in JSON");
+                }
+            }
+        }
+    }
+
+    public void add_more_than_four_user() throws PlayerExistsException, GameInProgressException, UnsupportedEncodingException, MaxPlayersReachedExeption, IOException {
+
+        String name = "any_name";
+        add_user(name, Boolean.TRUE);
+
+    }
+
+    public void add_same_name_user() throws PlayerExistsException, GameInProgressException, UnsupportedEncodingException, MaxPlayersReachedExeption, IOException {
+
+        String name = "any_name";
+        add_user(name, Boolean.FALSE);
+    }
+
+}
